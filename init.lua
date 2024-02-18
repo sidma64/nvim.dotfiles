3bfbe201698842ce729cb84c753b1b6e8314ad99
Uzay Toprak <toprak.code@gmail.com>
2024-02-18 21:14:18 +0300

    Added conform.nvim and keybinding to format

--- init.lua
+++ init.lua
@@ -1,23 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 -- This needs to be before lazy.nvim setup
-vim.o.mapleader = " "
-vim.wo.number = true
-require "lazy".setup(require "plugins")
+-- vim.cmd("nnoremap <SPACE> <Nop>")
+vim.opt.number = true
 
-vim.g.editorconfig = true
+require("lazy").setup(require("plugins"))
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+-- To set the Leader key in Neovim using Lua, you can add the following line to your init.lua file:
+vim.g.mapleader = " "
+
+-- Example mapping using the Leader key
 
 vim.cmd("colorscheme gruvbox")
 vim.cmd("set background=dark")
@@ -27,9 +32,33 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set autoindent")
 
+local wk = require("which-key")
 
-
-
-
-
--- example to setup lua_ls and enable call snippets
+wk.register({
+	["<leader>"] = {
+		p = {
+			name = "persistence.nvim",
+			l = {
+				function()
+					require("persistence").load({ last = true })
+				end,
+				"Load last session",
+			},
+			s = { require("persistence").load, "Restore session for the current directory" },
+			d = { require("persistence").stop, "Stop persistence.nvim" },
+		},
+		s = { "<cmd>Neotree<cr>", "Open neo-tree.nvim" },
+		q = {
+			name = "Quit commands",
+			q = { vim.cmd.quit, "Quit window" },
+			a = { vim.cmd.quitall, "Quit Vim" },
+		},
+		w = { vim.cmd.write, "Write" },
+		f = {
+			function()
+				require("conform").format({ bufnr = vim.api.nvim_get_current_buf() })
+			end,
+			"Format with conform.nvim",
+		},
+	},
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,93 +1,161 @@
 return {
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim",              cmd = "Neoconf" },
-  --  "folke/neodev.nvim",
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = true,
-    lazy = true,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require 'lspconfig'.lua_ls.setup {
-        on_init = function(client)
-          local path = client.workspace_folders[1].name
-          if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-              Lua = {
-                runtime = {
-                  -- Tell the language server which version of Lua you're using
-                  -- (most likely LuaJIT in the case of Neovim)
-                  version = 'LuaJIT'
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                  checkThirdParty = false,
-                  library = {
-                    vim.env.VIMRUNTIME
-                    -- "${3rd}/luv/library"
-                    -- "${3rd}/busted/library",
-                  }
-                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                  -- library = vim.api.nvim_get_runtime_file("", true)
-                }
-              }
-            })
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-          end
-          return true
-        end
-      }
-    end
-  },
-  'hrsh7th/nvim-cmp',
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-  {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", 'gomod' },
-    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require 'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "typescript", "javascript", "graphql", "c", "lua",
-          "vim", "vimdoc", "query", "go", "cmake", "cpp", "python" },
-        highlight = {
-          enable = true,
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = false
-        }
-      }
-    end
-  },
-  "editorconfig/editorconfig-vim",
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  }
+	"folke/which-key.nvim",
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	--  "folke/neodev.nvim",
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = true,
+		lazy = true,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("lspconfig").lua_ls.setup({
+				on_init = function(client)
+					local path = client.workspace_folders[1].name
+					if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+						client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+							Lua = {
+								runtime = {
+									-- Tell the language server which version of Lua you're using
+									-- (most likely LuaJIT in the case of Neovim)
+									version = "LuaJIT",
+								},
+								-- Make the server aware of Neovim runtime files
+								workspace = {
+									checkThirdParty = false,
+									library = {
+										vim.env.VIMRUNTIME,
+										-- "${3rd}/luv/library"
+										-- "${3rd}/busted/library",
+									},
+									-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+									-- library = vim.api.nvim_get_runtime_file("", true)
+								},
+							},
+						})
+						client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+					end
+					return true
+				end,
+			})
+		end,
+	},
+	"hrsh7th/nvim-cmp",
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- A list of parser names, or "all" (the five listed parsers should always be installed)
+				ensure_installed = {
+					"typescript",
+					"javascript",
+					"graphql",
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"go",
+					"cmake",
+					"cpp",
+					"python",
+				},
+				highlight = {
+					enable = true,
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = false,
+				},
+				indent = {
+					enable = false,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+	{
+		"nmac427/guess-indent.nvim",
+		config = function()
+			require("guess-indent").setup()
+		end,
+	},
+	-- Lua
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		opts = {
+			dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- directory where session files are saved
+			options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
+			pre_save = nil, -- a function to call before saving the session
+			save_empty = false, -- don't save if there are no open file buffers
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Conform will run multiple formatters sequentially
+					python = { "isort", "black" },
+					-- Use a sub-list to run only the first available formatter
+					javascript = { { "prettierd", "prettier" } },
+					cpp = { "clang_format" },
+					typescript = { { "prettierd", "prettier" } },
+					c = { "clang_format" },
+					go = { "gofmt" },
+				},
+			})
+		end,
+	},
 }
